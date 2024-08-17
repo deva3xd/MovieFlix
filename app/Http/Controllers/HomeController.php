2f1f1ca202ceab65683f8f48741c2d2ac342040b
deva3xd <deva3xd@gmail.com>
2024-08-18 00:19:59 +0700

    refactor: home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,16 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index() {
-        $res = Http::get('https://api.themoviedb.org/3/movie/now_playing?api_key=1768ded8bc2a22c8c989b581e2c276e6');
-        // dd($res->collect());
-        return Inertia::render('Home/Page', [
-            'res' => $res
-        ]);
+        return Inertia::render('Home/Page');
     }
 
     public function detail($id) {
