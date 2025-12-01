4ab65f6d8f2ea6ae37d4d17dd29dd37d8048bbc2
deva3xd <devarn05@gmail.com>
2025-12-01 21:50:57 +0700

    update file in routes folder

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
-use App\Http\Resources\MovieResource;
 use App\Models\Cart;
 
-// Route::get('/movie', function (Request $request) {
-//     $key = env('API_KEY');
-//     $url = env('API_URL');
-//     $response = Http::get("{$url}/now_playing?api_key={$key}");
-//     $data = MovieResource::collection($response->json()['results'])->toArray($request);
-
-//     return $data;
-// });
 
 Route::get('/cart', function () {
     $cart = Cart::all();
